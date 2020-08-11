bf5a66af6d9d3df7b279d9bcb3b304fd9eb9df0a
Patrick Brown <pjbrown05@gmail.com>
2020-08-11 10:02:18 -0500

    added virtual field

--- models/workout.js
+++ models/workout.js
@@ -32,7 +32,16 @@ const workoutSchema = new Schema({
       type: Number
     }
   }]
+},
+{
+toJSON: {virtuals: true}
 });
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration
+  }, 0)
+});
+
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
